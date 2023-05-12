80ed5f1a9263cd78fd405960ac2a383a6c6d77ed
hav4ik <hav4ik@users.noreply.github.com>
2023-05-12 16:54:21 -0700

    example

---
+++ sample_code/main.sh
@@ -0,0 +1,12 @@
+# Let's print some info
+echo "hello world";
+nvidia-smi;
+pip freeze;
+which python;
+
+# You can install packages here (it's better to create a curated environment,
+# but this way is easier for ad-hoc testing and debugging)
+pip install transformers;
+
+# execute train.py with first argument
+python train.py;
---
+++ sample_code/train.py
@@ -0,0 +1,32 @@
+from azureml.core import Run, Dataset, Workspace
+import os
+
+
+# get the Azure ML run object
+run = Run.get_context()
+
+# get workspace
+workspace = run.experiment.workspace
+
+# Mount the dataset to local path
+dataset = Dataset.get_by_name(workspace=workspace, name='sample_data')
+mount_context = dataset.mount()
+mount_context.start()
+data_path = mount_context.mount_point
+
+# List files in the dataset
+print(f"Files in dataset (mounted at {data_path}):")
+print(os.listdir(data_path))
+
+# Get default datastore
+datastore = workspace.get_default_datastore()
+
+# Create a sample file
+with open('sample_data.txt', 'w') as f:
+    f.write('This is sample data.')
+
+# Upload a file to the datastore. You can use this to checkpoint your model files
+datastore.upload_files(
+    files=['sample_data.txt'],
+    target_path='outputs/sample_upload',
+    overwrite=True)
